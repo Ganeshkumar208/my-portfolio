de57fc067622b39bcd9d0bfa7d9c553a9c559e36
Ganeshkumar208 <ganeshkumarakula17@gmail.com>
2024-10-07 16:11:54 +0530

    Form changes consoling the values and showing the alert

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,23 +1,74 @@
-import './FormStyles.css'
-
-import React from 'react'
+import './FormStyles.css';
+import React, { useState } from 'react';
 
 const Form = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form Data:', formData);
+        alert('Your details have been Reached to Mr.Ganesh. We will react to you soon!');
+        setFormData({
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        });
+    };
+
     return (
         <div className='form'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label className='txt'>Your Name</label>
-                <input type='text' />
+                <input
+                    type='text'
+                    name='name'
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+
                 <label className='txt'>Your Email</label>
-                <input type='email' />
+                <input
+                    type='email'
+                    name='email'
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+
                 <label className='txt'>Subject</label>
-                <input type='text' />
+                <input
+                    type='text'
+                    name='subject'
+                    value={formData.subject}
+                    onChange={handleChange}
+                />
+
                 <label className='txt'>Message</label>
-                <textarea rows={5} type='text' placeholder='Type Your Message' />
-                <button className='btns-form'>Submit</button>
+                <textarea
+                    rows={5}
+                    name='message'
+                    value={formData.message}
+                    onChange={handleChange}
+                    placeholder='Type Your Message'
+                />
+
+                <button className='btns-form' type='submit'>Submit</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default Form
+export default Form;
